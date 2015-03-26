7bbbd66dae89a9820453e70aa922ee162ea8c002
anthony-orlando <anthony-orlando@uiowa.edu>
2015-03-26 15:51:13 -0500

    Delete xDetails.aspx.vb

--- xDetails.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Partial Class Details
-    Inherits System.Web.UI.Page
-
-    Protected Sub DetailsView1_ItemDeleted(sender As Object, e As DetailsViewDeletedEventArgs) Handles DetailsView1.ItemDeleted
-        Response.Redirect("~/Default.aspx")
-    End Sub
-End Class
